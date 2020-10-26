050336a6027b3a02f844f692bf06b8c03b5a9615
Adrian <adcamilogg@gmail.com>
2020-10-26 02:47:49 -0500

    django debug toolbar is configured

--- src/MHNU/settings/local.py
+++ src/MHNU/settings/local.py
@@ -1,7 +1,17 @@
 from .base import *
+import socket
 
 DEBUG = True
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-ALLOWED_HOSTS = ['*']
+INSTALLED_APPS += ['debug_toolbar']
+
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
+
+DEBUG_TOOLBAR_CONFIG = {
+    "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
+    'SHOW_TOOLBAR_CALLBACK': lambda request: DEBUG
+}
